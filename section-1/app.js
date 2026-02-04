bfe842491ab2c16a19ed24a95c1372217e0fd518
SimonNguthu <simonnguthuM@gmail.com>
2026-02-04 19:18:47 +0300

    page

---
+++ section-1/app.js
@@ -0,0 +1,151 @@
+/* ===================== 
+Dom (Best practice at top) 
+===================== */
+const tableBody = document.getElementById("tableBody")
+const totalEmployees = document.getElementById("totalEmployees")
+const activeEmployees = document.getElementById("activeEmployees")
+const averageSalaryRange = document.getElementById("averageSalaryRange")
+const departmentsCount = document.getElementById("departmentsCount")
+const statusFilter = document.getElementById("statusFilter")
+const nextBtn = document.getElementById("nextBtn")
+const prevBtn = document.getElementById("prevBtn")
+
+let currentPage = 1
+let currentData = []
+
+/* ===================== 
+CONFIGURATION 
+===================== */
+const CONFIG = {
+  employeeNum: 50,
+  statuses: ["Active", "Inactive", "On Leave"],
+  departments: ["HR", "Sales"],
+  startDate: new Date("2010-01-01"),
+  endDate: new Date("2025-11-30"),  
+  pageSize: 10
+}
+
+/* ===================== 
+Utilities 
+===================== */
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+const randomDate = (start, end) => new Date(Math.random() * (end.getTime() - start.getTime()) + start.getTime())
+
+//Filter function
+function filterData (data, criteria) {
+  return data.filter(item => {
+    for(const key in criteria){
+      const rule = criteria[key]
+      if(!rule(item[key],item)) return false
+    }
+    return true
+  })
+}
+// {{{ Loop through each item in array: 
+// Loop through each rule: 
+// If any rule fails → discard item immediately 
+// If all rules pass → keep item }}}
+
+//Pagination function
+function paginate (data, page, pageSize) {
+  const start = (page - 1) * pageSize
+  const end = start + pageSize
+  return data.slice(start, end)
+}
+
+/* ===================== 
+Business Logic 
+===================== */
+function generateData (num) {
+  const employees = []
+
+  for (let i = 1; i <= num; i++){
+    let minSalary = randomNumber(30000, 50000)
+    let maxSlary = randomNumber(51000, 90000)
+
+    let employee = {
+      id: i,
+      name: `Employee ${i}`,
+      department: CONFIG.departments[randomNumber(0, CONFIG.departments.length-1)],
+      status: CONFIG.statuses[randomNumber(0, CONFIG.statuses.length-1)],
+      joinDate: randomDate(CONFIG.startDate, CONFIG.endDate),
+      salaryRange: `${minSalary} - ${maxSlary}`
+    }
+
+    employees.push(employee)
+  }
+  return employees
+}
+const employeeData = generateData(CONFIG.employeeNum)
+currentData = employeeData
+
+/* ===================== 
+UI 
+===================== */
+function renderTable (data) {
+  tableBody.innerHTML = ""
+
+  data.forEach(emp => {
+    tableBody.innerHTML += `
+      <tr>
+        <td>${emp.id}</td>
+        <td>${emp.name}</td>
+        <td>${emp.department}</td>
+        <td>${emp.status}</td>
+        <td>${emp.joinDate}</td>
+        <td>${emp.salaryRange}</td>
+      </tr>
+`
+  });
+}
+
+function updateCards (data) {
+  totalEmployees.innerHTML = data.length
+
+  activeEmployees.innerHTML = data.filter(emp   => emp.status === 'Active').length
+
+  averageSalaryRange.innerHTML = Math.round(data.reduce((sum, emp) => {
+    const [min, max] = emp.salaryRange.split(" - ").map(Number)
+    return sum + (max+min)/2
+  }, 0)/data.length)
+
+  departmentsCount.innerHTML = new Set(data.map(emp => emp.department)).size
+}
+
+function applyFilters () {
+  const criteria = {
+    status: val => statusFilter.value === "All" || val === statusFilter.value
+  }
+  currentData = filterData(employeeData, criteria)
+  currentPage = 1
+  render()
+}
+
+function nextPage () {
+  const totalPages = Math.ceil(currentData.length / CONFIG.pageSize)
+  if (currentPage < totalPages) {
+    currentPage++
+    render()
+  }
+}
+
+function prevPage () {
+  if (currentPage > 1) {
+    currentPage--
+    render()
+  }
+}
+
+function render () {
+  const pageData = paginate(currentData, currentPage, CONFIG.pageSize)
+  renderTable(pageData)
+  updateCards(currentData)
+}
+render()
+
+/* ===================== 
+Events 
+===================== */
+statusFilter.addEventListener("change", () => applyFilters())
+nextBtn.addEventListener("click", nextPage)
+prevBtn.addEventListener("click", prevPage)
